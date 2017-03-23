4c1ef223fa0f7ec0fa7241634e854c4a6f98904a
Joysome <khokhulyavladislav@gmail.com>
2017-03-23 08:04:36 +0200

    updated parsing method, created separate script for parser

--- js/Script.js
+++ js/Script.js
@@ -18,44 +18,5 @@ function init() {
 function generateTable() {
     var inputData = document.getElementById("csv-input").value;
     var tableContainer = document.getElementById("table-container");
-    var tableHTML = getTableHTML(inputData);
-    tableContainer.innerHTML = tableHTML;
-}
-
-//test
-function getTableHTML(inputData) {
-    var rows = inputData.split(/\r?\n|\r/);
-    
-    var table = '<table class="table table-hover">';
-    for (var row = 0; row < rows.length; row++) {
-        if (row === 0) {
-            table += '<thead>';
-            table += '<tr>';
-        } else {
-            table += '<tr>';
-        }
-        rows[row] = rows[row].split(',');
-        for (var cell = 0; cell < rows[row].length; cell++) {
-            if (row === 0) {
-                table += '<th>';
-                table += rows[row][cell];
-                table += '</th>';
-            } else {
-                table += '<td>';
-                table += rows[row][cell];
-                table += '</td>';
-            }
-        }
-        if (row === 0) {
-            table += '</tr>';
-            table += '</thead>';
-            table += '<tbody>';
-        } else {
-            table += '</tr>';
-        }
-    }
-    table += '</tbody>';
-    table += '</table>';
-
-    return table;
+    tableContainer.innerHTML = getAll(inputData);//test
 }
