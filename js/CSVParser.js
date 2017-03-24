0261eeecfb1cd17d54f954cbfc128ec4208375bd
Joysome <khokhulyavladislav@gmail.com>
2017-03-24 11:55:24 +0200

    patterns update & test parse func

--- js/CSVParser.js
+++ js/CSVParser.js
@@ -5,15 +5,28 @@
 //begin private closure
 (function () {
 
-    var _patternBeforeElem = "((?<=^)|(?<=\,)){1}";
-    var _pattenrAfterElem = "((?=\,)|(?=$)){1}";
-    var _patternNonQuoted = "(?:[^\,\"$])*";
-    var _patternDoubleQuoted = "\"+(?:[\s\S]|\"(?!\,)|(?<!\")\,)*\"+";
-    var _patternComplete = "((?<=^)|(?<=\,))+(\"+(?:[\s\S]|\"(?!\,)|(?<!\")\,)*\"+)|((?:[^\,\"$])*)((?=\,)|(?=$))+";
+    var _patternBeforeNonQuoted = "(?<=^|\,)";
+    var _patternBeforeQuoted = "(?<=(?<=^|\,)\")";
+    var _patternAfterNonQuoted = "(?=\,|$)";
+    var _pattenrAfterQuoted = "(?=\"{1}(?=\,|$))";
+    var _patternNonQuoted = "(?:[^\,\"\r\n])*";
+    var _patternQuoted = "(?:[^\"]|\"(?!(\,|$)))*";
+    var _patternComplete = "((?<=(?<=^|\,)\")(?:[^\"]|\"(?!(\,|$)))*(?=\"{1}(?=\,|$))|((?<=^|\,)(?:[^\,\"\r\n])*(?=\,|$)))";
 
     //This function should return a [[]] array or null for error
     this.parse = function (string) {
+        var res;
+        var pattern = _patternComplete;
+        var regex = new RegExp(pattern, "g");
 
+        while ((res = regex.exec(string)) !== null) {
+            //infinite loop fix
+            if (res.index === regex.lastIndex) {
+                regex.lastIndex++;
+            }
+            alert("Найдено " + res + " at index "
+            + res.index + ".\nNext symbol index: " + pattern.lastIndex);//test
+        };
     };
 
 }).call(CSVParser);
